641e19f84961a3c04cee0b8b297d937c2b038978
Your Name <you@example.com>
2022-10-23 20:13:25 -0500

    Front APP only, stores user and pass on DB ASTRA-PASS hashed secured

---
+++ server.js
@@ -0,0 +1,54 @@
+const PORT = 8000
+require('dotenv').config()
+const express = require('express')
+const axios = require ('axios')
+const cors = require('cors')
+const {v4: uuidv4} = require('uuid')
+const bcrypt = require('bcrypt')
+const path = require('path')
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+
+
+
+
+app.post('/signup', async (req, res) =>{
+    const { userName, password } = req.body
+    console.log(userName)
+    console.log(password)
+
+    const userId = uuidv4()
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    /* To check if hash pass is equal to  the password the user entered in the form
+    const success = await bcrypt.compare("password", "hashedPassword")    -----> keep the "" just replace with the pass and has from db
+    
+    console.log('is sucess?', success);*/
+
+    const options = {
+        method: "POST",
+        headers: {
+            Accepts: "application/json",
+            "X-Cassandra-Token": process.env.ASTRA_TOKEN,
+            "Content-Type": "application/json"
+        },
+        data: {userName, userId, hashedPassword}
+    }
+
+    try {
+        const response = await axios(process.env.ASTRA_URL, options)
+        res.status(200).json(response.data)
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({message: err})
+    }
+
+})
+app.get('*', (req, res) =>{
+    res.sendFile(path.resolver(__dirname, "build", "index.html"))
+})
+
+app.listen(PORT, () => console.log("Server is running in port: " + PORT))
--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import './App.css'
+import axios from 'axios'
 
 function App() {
+  const [userName, setUserName] = useState(null)
+  const [password, setPassword] = useState(null)
+  const [confirmPassword, setConfirmPassword] = useState(null)
+  const [error, setError] = useState(null)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (password !== confirmPassword) {
+      setError('** Make sure your passwords match!!!!')
+      return
+    }
+
+    axios.post('/signup', {
+      userName,
+      password
+    })
+
+    console.log('userName',userName);
+    console.log('password',password);
+    console.log('confirmPassword',confirmPassword);
+    setError(null)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body>
+      <div className="center">
+      <h1>Sign Up</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="txt_field">
+          <input type="text" required
+          placeholder="Username"
+          onChange={(e) => setUserName(e.target.value)}
+          />
+          <span></span>
+        </div>
+
+        <div className="txt_field">
+          <input type="password" required
+          placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
+          />
+          <span></span>
+        </div>
+
+        <div className="txt_field">
+          <input type="password" required
+          span
+          placeholder="Check-Password"
+          onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+          <span></span>
+        </div>
+        <input type="submit"/>
+        <p>{error}</p>
+      </form>
+      </div>
+    </body>
   );
 }
 
